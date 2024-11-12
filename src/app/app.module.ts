7a87852b82fe68c2414fad5744b0591e6530abf9
smccarthy-pm <smccarthy@advisor360.com>
2024-11-12 12:49:00 -0500

    fixing app module ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { FinancialAdvisorInterfaceComponent } from './financial-advisor-interface/financial-advisor-interface.component';
+import { FinancialAdvisorInterfaceComponent } from './components/financial-advisor-interface/financial-advisor-interface.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    FinancialAdvisorInterfaceComponent // Make sure to declare the component here
+    FinancialAdvisorInterfaceComponent, // Make sure to declare the component here
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
